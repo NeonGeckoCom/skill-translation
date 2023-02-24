72ee15dfeaa4a1d40bb860afe549c60c9235ef16
Daniel McKnight <daniel@neongecko.com>
2023-02-23 17:55:24 -0800

    Correct RuntimeRequirements

--- __init__.py
+++ __init__.py
@@ -52,11 +52,11 @@ class Translation(NeonSkill):
         return RuntimeRequirements(network_before_load=False,
                                    internet_before_load=False,
                                    gui_before_load=False,
-                                   requires_internet=False,
-                                   requires_network=False,
+                                   requires_internet=True,
+                                   requires_network=True,
                                    requires_gui=False,
-                                   no_internet_fallback=True,
-                                   no_network_fallback=True,
+                                   no_internet_fallback=False,
+                                   no_network_fallback=False,
                                    no_gui_fallback=True)
 
     @property
